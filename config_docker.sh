10eead5c70c2843708a70615a2ce7765971460e5
Enrico Alberti <s279434@studenti.polito.it>
2022-07-27 17:21:30 +0200

    added docker configuration for controller node

---
+++ config_docker.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Installing Docker..."
+sudo curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+echo "Waiting for Docker to launch..."
+while (! sudo docker stats --no-stream ); do
+  # Docker takes a few seconds to initialize
+  echo "Waiting for Docker to launch..."
+  sleep 1
+done
+
+echo "Downloading Mystique Image"
+sudo docker pull enrico2docker/ubuntu-mystique:1.1
+
+echo "Creating Mystique Container"
+sudo docker run --name mystique -p 6633:6633 -d -t enrico2docker/ubuntu-mystique:1.1
+
+# config Ryu
+# data_frequency= ' 1'
+# support_switches=' s0'
+# cmd_config_ryu = '/bin/bash /mystique/ryu_controller/config_ryu.sh'+data_frequency+support_switches #config.ini - ryu_controller
+# cmd_docker_ryu = 'sudo docker exec -d mystique ' + cmd_config_ryu
+
+
+# config Model
+
+#run ryu
+
+#run train
