f4c5bec6d58f447de1f2439bd15b12dd51d8005a
Enrico Alberti <s279434@studenti.polito.it>
2022-08-02 18:14:12 +0200

    creation of ovs; setting IPctrl

--- config_ovs.sh
+++ config_ovs.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-#It's supposed that the address for the hosts and switches goes from .0.1 to .6.255
-#So I'm skipping from .7.0 to .7.255 which are the link to controller.
+# It's supposed that the address for the hosts 
+# and switches goes from .0.1 to .6.255
 INTFS=$(ip address | grep 192.168.[0-6])
 
 SW=$(ls -A /sys/class/net | grep s[0-9] )
 NUM_INTFS=$(ls -A /sys/class/net | wc -l)
 
+IP_CTRL=${1}
+
+#Creating OVS switch
+sudo ovs-vsctl add-br $SW
+
+# Disabling interfaces from node and Mapping the 
+# interfaces from node to OVS
 for ((i=0; i < $NUM_INTFS; i++)); do
     INTF='eth'$i
     if [[ $INTFS =~ $INTF ]]
@@ -15,3 +22,7 @@ for ((i=0; i < $NUM_INTFS; i++)); do
         sudo ovs-vsctl add-port $SW $INTF
     fi
 done
+
+#setting the IP of the controller
+sudo ovs-vsctl set-controller $SW tcp:$IP_CTRL:6633
+sudo ovs-vsctl set-fail-mode $SW secure
